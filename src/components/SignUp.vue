18320f9c86ce7dfefd3e9ed15d4c779bf6c81054
DMTRS <dmytro.bulankin@gmail.com>
2025-05-28 21:33:42 +0200

    Final implementation

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,10 +3,6 @@
     <div
       class="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24"
     >
-      <p class="flex justify-center">
-        <img class="h-28 w-auto" src="/Logo.png" alt="Tasky Project Logo" />
-      </p>
-
       <div class="mx-auto w-full max-w-sm lg:w-96">
         <div>
           <h2
